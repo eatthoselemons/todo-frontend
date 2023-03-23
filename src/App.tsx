3865317b34bb9194c8801834050b9cbf5ab40c24
Liz Lambert <preevete@gmail.com>
2023-03-22 21:46:29 -0700

    Initial frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,82 @@
-import React, {
-  ChangeEventHandler,
-  createRef,
-  FormEventHandler,
-  useState,
-} from "react";
+import React, { createRef, FormEventHandler, useEffect, useState } from "react";
 
-import "./App.css";
-
-import {
-  Checkbox,
-  Container,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  TextField,
-} from "@mui/material";
-import * as uuid from "uuid";
-import { Delete } from "@mui/icons-material";
-
-class Item {
-  subItems: Array<Item> = [];
-  id: string = uuid.v4();
-  constructor(public text: string, public done: boolean = false) {}
-}
+import { Box, Container, Fab, TextField, Typography } from "@mui/material";
+import TaskList from "./components/TaskList";
+import { Task } from "./domain/Task";
+import { createTask, getRootTasks } from "./service/TaskService";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
-  const [items, setItems] = useState<Array<Item>>([]);
-
   const newItem = createRef<HTMLInputElement>();
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    getRootTasks().then(setTasks);
+  }, []);
 
   const handleNewItem: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     if (newItem.current?.value) {
-      setItems(items.concat([new Item(newItem.current.value)]));
+      const newTask = new Task(newItem.current.value);
+      setTasks([...tasks, newTask]);
+      // TODO Handle async nature with spinner?
+      // TODO handle adding tasks to non root
+      createTask("root", newTask);
+
       newItem.current.value = "";
     }
   };
 
-  const handleItemChecked: ChangeEventHandler<HTMLInputElement> = (e) => {};
-
-  const ItemsComponent: React.FC<{ items: Item[] }> = ({ items }) => {
-    if (items.length === 0) {
-      return <></>;
-    }
-    return (
-      <List>
-        {items.map((item, index) => (
-          <ListItem
-            key={item.id}
-            secondaryAction={
-              <IconButton edge="end" aria-label="delete">
-                <Delete />
-              </IconButton>
-            }
-          >
-            <ListItemButton>
-              <Checkbox
-                checked={item.done}
-                disableRipple
-                tabIndex={-1}
-                edge="start"
-                inputProps={{ "aria-labelledby": `${item.id}-text` }}
-                onChange={(e) => {
-                  item.done = e.target.checked;
-                  setItems(items);
-                }}
-              />
-
-              <ListItemText id={`${item.id}-text`}>{item.text}</ListItemText>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    );
-  };
-
   return (
-    <Container>
+    <Container
+      sx={{
+        position: "relative",
+        minHeight: "100vh",
+      }}
+    >
       <header>
-        <h1>Liz'z Lemons</h1>
+        <Typography variant="h3">Liz'z Lemons</Typography>
       </header>
       <main>
-        <ItemsComponent items={items} />
-        <form onSubmit={handleNewItem}>
-          <TextField
-            label="New Item"
-            variant="standard"
-            autoFocus
-            autoComplete="on"
-            autoCapitalize="words"
-            inputRef={newItem}
-          />
-        </form>
+        <TaskList tasks={tasks} />
       </main>
+      <footer>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+          }}
+        >
+          <Box
+            component="form"
+            onSubmit={handleNewItem}
+            sx={{
+              flex: "1",
+            }}
+          >
+            <TextField
+              label="New Item"
+              variant="standard"
+              autoFocus
+              autoComplete="on"
+              autoCapitalize="words"
+              inputRef={newItem}
+              fullWidth={true}
+            />
+          </Box>
+          <Fab
+            color="primary"
+            sx={{
+              m: 1,
+              mr: 0,
+            }}
+          >
+            <AddIcon />
+          </Fab>
+        </Box>
+      </footer>
     </Container>
   );
 }
