5f2229587eea59c5be689d097aa7eb455627a08e
Liz Lambert <preevete@gmail.com>
2023-04-22 23:46:37 -0700

    webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
-const stylesHandler = "style-loader";
-
 const config = {
   entry: "./src/index.tsx",
   output: {
